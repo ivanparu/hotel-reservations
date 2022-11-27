422c6f0576cde0c864babfc0e755b19ac8ad0a70
Ivan Parunov <ivanparu04@gmail.com>
2022-11-27 00:30:16 -0300

    agregando html

--- Habitacion.js
+++
@@ -1,7 +0,0 @@
-class Habitacion{
-    constructor(id,nombre,precio ){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = Number(precio);
-    }
-}
--- main.js
+++ main.js
@@ -1,74 +1,167 @@
-
-const arrayHabitaciones = [];
-
-const habitacion1 = new Habitacion (1, 'suite', 2000);
-const habitacion2 = new Habitacion (2, 'ejecutiva', 1000);
-const habitacion3 = new Habitacion (3, 'deluxe', 5000);
-
-arrayHabitaciones.push(habitacion1, habitacion2, habitacion3);
-
-const mostarOrdenado = () => {
-    let array = [];
-    arrayHabitaciones.forEach(habitacion => array.push(habitacion.nombre+ ' $' +habitacion.precio));
-    alert('Lista de precios:'+ '\n'+array.join('\n'));
-}
-
-const menorAMayor = () => {
-    arrayHabitaciones.sort((a,b) => a.precio - b.precio);
-    mostarOrdenado();
+const buscarHabitacion = (habitacionNombre) => {
+    const habitacionesBusqueda = habitaciones.filter( habitacion => habitacion.nombre.toLowerCase().includes(habitacionNombre.toLowerCase()) || habitacion.desc.toLowerCase().includes(habitacionNombre.toLowerCase()));
+    mostrarHabitaciones(habitacionesBusqueda);
 };
 
-function reservarHabitacion(){
-    let habitacion = '';
-    let cantidad = 0;
-    let total = 0;
-    let seguirComprando = true;
+
+const mostrarHabitaciones = (habitaciones) => {
+    const contenedorHabitacion = document.getElementById("habitacion-contenedor");
+
+    contenedorHabitacion.innerHTML= "";
+
+    habitaciones.forEach(habitacion => {
+    const div = document.createElement('div');
+    div.style = `width : 18rem`;
+    div.classList.add('card');
+            div.innerHTML += `
+                                    <img src="${habitacion.img}" class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">${habitacion.nombre}</h5>
+                                    <p class="card-text">${habitacion.desc}</p>
+                                    <a href="#" class="btn btn-primary" id="boton${habitacion.id}">Comprar</a>
+                                </div>
+                    `
+    contenedorHabitacion.appendChild(div);
 
 
-    do{
-        habitacion = parseInt(prompt('Que habitación queres?\n 1:suite 2:ejecutiva 3:deluxe', 'Ej: 1'));
 
-        const habitacionSeleccionada = arrayHabitaciones.find(buscarHabitacion => buscarHabitacion.id === habitacion)
-        
-        if(!habitacionSeleccionada){
-            alert('La habitacion no se encuentra disponible.');
-            continue;
-        }
-        cantidad = parseInt(prompt('Cuantas días?'));
-        
-        let cantidadValidada = validarCantidad(cantidad);
-        
-        total += habitacionSeleccionada.precio * cantidadValidada;
-        alert('Lo que te sale es $' + total);
-        
-        seguirComprando = confirm('Queres seguir comprando?')
-
-} while(seguirComprando)
-return total;
-}
+    const boton = document.getElementById(`boton${habitacion.id}`);
+    boton.addEventListener("click", () => {
+        agregarAlCarrito(habitacion.id);
+    });
+    });
+}; 
 
 
-function validarCantidad(cantidad){
-    while(Number.isNaN(cantidad) || cantidad === 0) {
-        if(cantidad !== 0){
-            alert('Tenes que poner un numero');
-        } else{ 
-            alert('Tenes que poner un numero distinto de 0');
-        } cantidad = parseInt(prompt('Cuantas queres?'))
-    } return cantidad;
-}
+let carrito = [];
 
-function descuento (total){
-    let lugarDeResidencia = prompt('Sos de Buenos Aires?', 'Si o no').toLocaleLowerCase();
-    if(lugarDeResidencia === 'si'){
-        alert('Entonces te hacemos un descuento del 15%, por lo tanto el total es de: $' +  total * 0.85) ;
-    } else if (lugarDeResidencia === 'no'){
-        alert('Como no sos de Buenos Aires no te hacemos descuento por residencia, por lo tanto el total es de: $' + total)
-    } else{
-        alert('Tenes que poner si o no');
-        descuento(total);
+const agregarAlCarrito = (habitacionId) => {
+    if (chequearHabitacion(habitacionId)) {
+        Toastify({
+            text: `No se pueden comprar dos habitaciones iguales`,
+            position: "center",
+            duration: 1000
+        }).showToast();
+
+    } else {
+        const habitacion = habitaciones.find( habitacion => habitacion.id === habitacionId );
+        carrito.push(habitacion)
+        Toastify({
+            text: `Se agregó el ${habitacion.nombre} al carrito`,
+            duration: 2000,
+            position: "center"
+        }).showToast();
+    
+        actualizarTotal(carrito)
+        mostarCarrito(carrito)
+        guardarCarritoStorage();
     }
 }
 
-menorAMayor()
-descuento(reservarHabitacion());
+const modalContenedor = document.querySelector('.modalContenedor');
+const abrirCarrito = document.getElementById('abrirCarrito');
+const cerrarCarrito = document.getElementById('btnCerrarCarrito');
+const modalCarrito = document.querySelector('.modalCarrito')
+
+abrirCarrito.addEventListener('click', () => {
+    modalContenedor.classList.toggle('modal-active')
+});
+
+cerrarCarrito.addEventListener('click', () => {
+    modalContenedor.classList.toggle('modal-active')
+});
+
+modalContenedor.addEventListener('click', () => {
+    cerrarCarrito.click()
+});
+
+modalCarrito.addEventListener("click", (e) => {
+    e.stopPropagation();
+    if (e.target.classList.contains('boton-eliminar')) {
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: 'Va a eliminar la habitacion de su carrito',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Eliminar',
+            cancelButtonText: 'Cancelar',
+            icon: 'warning',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                eliminarHabitacionCarrito(e.target.value);
+                Swal.fire(
+                    'Eliminado',
+                    'La habitacion ha sido eliminado',
+                    'success'
+                )
+            }
+        })
+    }
+});
+
+const eliminarHabitacionCarrito = (habitacionId) => {
+    const carritoStorage = obtenerCarritoStorage();
+    let carritoActualizado = carritoStorage.filter( habitacion => habitacion.id != habitacionId);
+
+    actualizarTotal(carritoActualizado)
+
+    carrito = carritoActualizado
+    mostarCarrito(carritoActualizado);
+    guardarCarritoStorage();
+};
+
+const mostarCarrito = (habitaciones) => {
+    const contenedor = document.getElementById("carrito-contenedor");
+    contenedor.innerHTML = ""
+    habitaciones.forEach(habitacion =>{
+        contenedor.innerHTML += `<div class="habitacionEnCarrito">
+                                    <p>${habitacion.nombre}</p>
+                                    <p>Precio: ${habitacion.precio}</p>
+                                    <button id="eliminar${habitacion.id}" class="btn waves-effect waves-ligth boton-eliminar" value="${habitacion.id}">X</button>
+                                </div> `}
+                                )
+}
+
+const actualizarTotal = (carritoActualizado) => {
+    let totalCompra = 0
+    carritoActualizado.forEach(habitacion => {
+        totalCompra += habitacion.precio
+    })
+
+    const precioTotal = document.getElementById("precioTotal");
+    precioTotal.innerHTML = totalCompra;
+
+};
+
+const guardarCarritoStorage = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+const obtenerCarritoStorage = () => {
+    const carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+    return carritoStorage;
+}
+
+const chequearHabitacion = (habitacionId) =>{
+    if (localStorage.getItem("carrito")) {
+        carrito = obtenerCarritoStorage();
+    };
+
+    const habitacionRepetida = carrito.find( habitacion => habitacion.id === habitacionId);
+    return habitacionRepetida
+}
+
+mostrarHabitaciones(habitaciones);
+
+if (localStorage.getItem("carrito")) {
+    carrito = obtenerCarritoStorage();
+};
+mostarCarrito(carrito);
+actualizarTotal(carrito);
+
+const inputSearch = document.getElementById("buscarHabitacion");
+
+inputSearch.addEventListener("input", (e) => {
+    buscarHabitacion(e.target.value);
+});
